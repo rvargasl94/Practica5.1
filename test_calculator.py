b4d671e48870f0811fa20e7219aec9b09e146c57
rvargasl <rubenwallapop94@gmail.com>
2025-03-19 12:58:07 +0100

    Introducir error en las pruebas unitarias

--- test_calculator.py
+++ test_calculator.py
@@ -8,5 +8,11 @@ class TestCalculadora(unittest.TestCase):
         self.assertEqual(calc.multiplicar(-1, 1), -1)
         self.assertEqual(calc.multiplicar(0, 5), 0)
 
+    def test_sumar(self):
+        calc = Calculadora()
+        self.assertEqual(calc.sumar(2, 3), 6)  # Error: 2 + 3 = 5, no 6
+        self.assertEqual(calc.sumar(-1, 1), 0)
+        self.assertEqual(calc.sumar(0, 5), 5)
+
 if __name__ == "__main__":
     unittest.main()
